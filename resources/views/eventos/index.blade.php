92bb0aec4b7989a9a09c7a1711e19516dac67360
Leonel Omar Gonzales Coronacion <leonelomar_gc@hotmail.com>
2016-09-22 18:42:47 -0500

    module client

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -31,7 +31,16 @@
                             <h4 class="pull-right">Hay {!! $eventos->count() !!} registros en la vista</h4>
                         </div>
                     </div>
-                    <br>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="well text-center">
+                                @foreach($compromisos as $compromiso)
+                                <a href="{{route('filter',[$compromiso->nombre,'id'=>$compromiso->id])}}" class="btn btn-primary btn-xs" style="margin: 2px;">{{$compromiso->nombre}} <span class="badge">{{$compromiso->cantidad}}</span></a>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+
                     @foreach($eventos as $evento)
                         <div class="col-sm-5 col-md-3">
                             <div class="thumbnail">
@@ -43,6 +52,7 @@
                                     @else
                                         <p>{{$evento->nombre}}</p>
                                     @endif
+                                    <small>{{$evento->fecha}}</small>
                                     <p><a href="{{route('admin.evento.show',$evento->id)}}" class="btn btn-primary" role="button">Ver</a></p>
                                 </div>
                             </div>
@@ -59,24 +69,75 @@
                 <div class="panel panel-default">
                     <div class="panel-heading"><i class="fa fa-pencil-square-o"></i> Busquedas</div>
                     <div class="panel-body">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                {!! Form::open(['route'=>'admin.evento.index','method'=>'GET','role'=>'form']) !!}
-                                {!! Form::label('search','Buscar por lugar') !!}
-                                {!! Form::text('lugar',null,['class'=>'form-control','id'=>'search','placeholder'=>'lugar de evento']) !!}
-                                <br>
-                                {!! Form::submit('Buscar',['class'=>'btn btn-primary pull-right']) !!}
-                                {!! Form::close() !!}
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title">
+                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            Buscar por lugar
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                {!! Form::open(['route'=>'admin.evento.index','method'=>'GET','role'=>'form']) !!}
+                                                {!! Form::label('search','Buscar por lugar') !!}
+                                                {!! Form::text('lugar',null,['class'=>'form-control','id'=>'search','placeholder'=>'lugar de evento']) !!}
+                                                <br>
+                                                {!! Form::submit('Buscar',['class'=>'btn btn-primary pull-right btn-xs']) !!}
+                                                {!! Form::close() !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                {!! Form::open(['route'=>'clientes','method'=>'GET','role'=>'form']) !!}
-                                {!! Form::label('cliente','Buscar por cliente') !!}
-                                {!! Form::text('cliente',null,['class'=>'form-control','id'=>'cliente','placeholder'=>'nombre del cliente']) !!}
-                                <br>
-                                {!! Form::submit('Buscar',['class'=>'btn btn-primary pull-right']) !!}
-                                {!! Form::close() !!}
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingTwo">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                            Buscar por cliente
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                {!! Form::open(['route'=>'clientes','method'=>'GET','role'=>'form']) !!}
+                                                {!! Form::label('cliente','Buscar por cliente') !!}
+                                                {!! Form::text('cliente',null,['class'=>'form-control','id'=>'cliente','placeholder'=>'nombre del cliente']) !!}
+                                                <br>
+                                                {!! Form::submit('Buscar',['class'=>'btn btn-primary pull-right btn-xs']) !!}
+                                                {!! Form::close() !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingThree">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                            Buscar por filmador
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                {!! Form::open(['route'=>'clientes','method'=>'GET','role'=>'form']) !!}
+                                                {!! Form::label('cliente','Buscar por filmador') !!}
+                                                {!! Form::text('cliente',null,['class'=>'form-control','id'=>'cliente','placeholder'=>'nombre del cliente']) !!}
+                                                <br>
+                                                {!! Form::submit('Buscar',['class'=>'btn btn-primary pull-right btn-xs']) !!}
+                                                {!! Form::close() !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
